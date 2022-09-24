721dc3977243bd305235188ff5ffe6c1e0d59b07
youwei828 <641531008@qq.com>
2022-09-24 17:04:58 +0800

    vue

---
+++ Vue/vue/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+    data() {
+        return {
+            title: 'hd',
+        };
+    },
+};
+</script>
+
+<template>
+    <h1 class="hd">{{ title }}</h1>
+
+    <!-- <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style>
+.hd {
+    background-color: aquaxxxxxxxxxxxxxxxxx;
+}
+/* .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+} */
+</style>
---
+++ Vue/vue/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import hd from './assets/hd.scss';
+createApp(App).mount('#app');
