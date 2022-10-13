0251361d3151d4a485025b51a8051668cf051dba
youwei828 <641531008@qq.com>
2022-10-13 23:13:04 +0800

    promise

--- leetCode/1.两数之和.js
+++ leetCode/1.两数之和.js
@@ -6,9 +6,20 @@
 
 // @lc code=start
 /**
- * @param {number[]} nums
+ * @param {number} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+    return [];
+};
 // @lc code=end
