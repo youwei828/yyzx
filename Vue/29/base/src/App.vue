7af996f10813f929883b5a1b7d4c9ce089b8ba34
youwei828 <641531008@qq.com>
2022-10-27 15:50:47 +0800

    keyfrom

--- Vue/29/base/src/App.vue
+++ Vue/29/base/src/App.vue
@@ -1,7 +1,95 @@
+<script setup>
+import { ref } from 'vue'
+import Anm from './components/Anm.vue'
+import Trans from './components/Trans.vue'
+const show = ref(false)
+</script>
 <template>
-    <div>1</div>
+    <!-- <anm v-if="show">保存提交</anm> -->
+    <!-- 需要用transition标签,并定义三个类 -->
+    <Transition>
+        <trans v-if="show"> 过渡 </trans>
+    </Transition>
+    <button @click="show = !show">切换</button>
 </template>
+<style lang="scss" scoped>
+button {
+    display: block;
+    margin-top: 20px;
+}
+// 优化后的动画样式
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+    transform: translateX(200px);
+}
+.v-enter-active,
+.v-leave-active {
+    transition: 2s ease;
+}
+.v-enter-to,
+.v-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+}
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+// 消失--过渡--出现
+// 定义过初始什状态
+// .v-enter-from {
+//     opacity: 0;
+//     transform: translateX(200px);
+// }
+// // 定以过渡中间世间以及样式
+// .v-enter-active {
+//     transition: 2s ease;
+// }
+// // 定义过渡达到的目标状态
+// .v-enter-to {
+//     opacity: 1;
+//     transform: translateX(0);
+// }
+// // 出现--过渡--消失
+// .v-leave-from {
+//     opacity: 1;
+// }
+// .v-leave-active {
+//     transition: 2s ease;
+//     transition-duration: 2s;
+// }
+// .v-leave-to {
+//     opacity: 0;
+//     transform: translateX(200px);
+// }
+// div {
+//     background-color: lightblue;
+//     padding: 20px;
+//     animation-name: tranx;
+//     animation-duration: 2s;
+//     animation-direction: alternate;
+//     transition-timing-function: ease;
+//     animation-iteration-count: infinite;
+// }
+// button {
+//     margin-top: 50px;
+// }
+// @keyframes tranx {
+//     0% {
+//         transform: translateX(-5px);
+//     }
+//     20% {
+//         transform: translateX(5px);
+//     }
+//     40% {
+//         transform: translateX(-5px);
+//     }
+//     60% {
+//         transform: translateX(5px);
+//     }
+//     80% {
+//         transform: translateX(-5px);
+//     }
+//     100% {
+//         transform: translateX(0px);
+//     }
+// }
+</style>
---
+++ Vue/29/base/src/components/Anm.vue
@@ -0,0 +1,72 @@
+<!-- <script setup>
+import { ref } from 'vue'
+const show = ref(false)
+</script> -->
+<template>
+    <section>
+        <div><slot></slot></div>
+    </section>
+    <!-- <button @click="show = !show">切换</button> -->
+</template>
+<style lang="scss" scoped>
+section {
+    perspective: 800px;
+}
+div {
+    display: inline-block;
+    border-radius: 8px;
+    background-color: lightblue;
+    padding: 10px 20px;
+    // animation-name: tranx;
+    animation-duration: 2s;
+    transition: 1s;
+    // animation-direction: alternate;
+    transition-timing-function: ease;
+    // animation-iteration-count: infinite;
+    cursor: pointer;
+    &:hover {
+        // transform: rotateY(360deg) rotateX(360deg);
+        background-color: lightslategrey;
+        color: whitesmoke;
+        animation-name: trany;
+        animation-direction: alternate;
+        animation-fill-mode: forwards;
+        transition: 1s;
+        animation-duration: 0.5s;
+    }
+}
+button {
+    margin-top: 70px;
+}
+@keyframes tranx {
+    0% {
+        transform: translateX(-5px);
+    }
+    20% {
+        transform: translateX(5px);
+    }
+    40% {
+        transform: translateX(-5px);
+    }
+    60% {
+        transform: translateX(5px);
+    }
+    80% {
+        transform: translateX(-5px);
+    }
+    100% {
+        transform: translateX(0px);
+    }
+}
+@keyframes trany {
+    0% {
+        transform: translateY(0px);
+    }
+    50% {
+        transform: translateY(-7px);
+    }
+    100% {
+        transform: translateY(0px);
+    }
+}
+</style>
