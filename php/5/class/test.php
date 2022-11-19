0b0cb3fef4f26c925a85c2945ab73e461dae8acb
youwei828 <641531008@qq.com>
2022-11-19 16:39:25 +0800

    2022.11.19

--- php/5/class/test.php
+++ php/5/class/test.php
@@ -7,6 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>验证码测试</title>
 </head>
+<style>
+.code {
+    width: 80px;
+    height: 10px;
+    /* border: 2px red solid; */
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+</style>
 
 <body>
     <form action="1.php" method="POST">
@@ -15,8 +25,7 @@
                 <td><input type="text" name="enter"></td>
             </tr>
             <tr>
-                <td style="cursor:pointer ;"><img src="./code.php" alt="验证码"
-                        onclick="this.src = 'code.php?'+Math.random()"></td>
+                <td class="code"><img src="./code.php" alt="验证码" onclick="this.src = 'code.php?'+Math.random()"></td>
             </tr>
             <tr>
                 <td>
